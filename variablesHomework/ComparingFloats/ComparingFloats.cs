2f6c2535c782469a373905b70b91b22c5ca3f7e5
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-01-11 14:18:54 +0200

    Precision

---
+++ variablesHomework/ComparingFloats/ComparingFloats.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparingFloats
+{
+    class ComparingFloats
+    {
+        static void Main(string[] args)
+        {
+            double precision = 0.000001;
+            char[] presitionA = precision.ToString().ToCharArray();
+            Console.WriteLine("Enter a number a");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number b = ");
+            double b = double.Parse(Console.ReadLine());
+            double substratction = a - b;
+
+           
+            for (int i = 0; i < presitionA.Length; i++)
+            {
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

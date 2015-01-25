b8fdbb08106371eebd09088a7e7f6bf0ef942ef0
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-01-25 16:24:16 +0200

    numbers

--- ConsolInOut/FormatingNumbers/FormatingNumbers.cs
+++ ConsolInOut/FormatingNumbers/FormatingNumbers.cs
@@ -27,29 +27,71 @@
             string a = Console.ReadLine();
             int numberA;
             bool isNumberA = int.TryParse(a, out numberA);
-
-            Console.WriteLine("Enter the second number");
-            string b = Console.ReadLine();
-            float numberB;
-            bool isNumberB = float.TryParse(b, out numberB);
-
-            Console.WriteLine("Enter the second number");
-            string c = Console.ReadLine();
-            float numberC;
-            bool isNumberC = float.TryParse(c, out numberB);
-            //Validationg the numbers
-            if (isNumberA && (isNumberB&&isNumberC))
+            if (isNumberA && numberA>=0&&numberA<=500)
             {
                 //Converting and formating 
-                int hexaA = Convert.ToInt32(a, 16);
-                string binaryA = Convert.ToInt32(a, 2).ToString();
-                Console.WriteLine("{0}", hexaA);
-                Console.WriteLine("Thank you for using the software have a nice Day :)");
+                Console.WriteLine("The input number {0}  is corectly entered ", a);
             }
             else
             {
                 Console.WriteLine("You put wrong niumber . Pleas put  an integer ");
             }
+            Console.WriteLine("Enter the second number");
+            Console.Write("\n");
+            string b = Console.ReadLine();
+            float numberB;
+            bool isNumberB = float.TryParse(b, out numberB);
+             //Validationg the numbers
+            if (isNumberB)
+            {
+                //Converting and formating 
+
+                Console.WriteLine("The input number {0} is corectly entered ", b);
+            }
+            else
+            {
+                Console.WriteLine("You put wrong niumber .");
+            }
+            Console.Write("\n");
+            Console.WriteLine("Enter the tird number");
+            string c = Console.ReadLine();
+            float numberC;
+            bool isNumberC = float.TryParse(c, out numberC);
+            //Validationg the numbers
+            if (isNumberC)
+            {
+                //Converting and formating 
+                Console.WriteLine("The input number {0} is corectly entered ", c);
+            }
+            else
+            {
+                Console.WriteLine("You put wrong niumber . Pleas put  an integer ");
+            }
+
+            Console.Write("\n");
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("The program now converst the digits that you entered ");
+
+            Console.Write("\n");
+            //Converting first namber to Hex and binary.
+        
+            Console.WriteLine("The first number {0} the second {1} the tird {2} are converted as  ", numberA,numberB,numberC);
+
+            //converting A to binay 
+            string resultHexa = String.Format("{0:X}", numberA);
+            string resultAinBinary = Convert.ToString(numberA, 2);
+            //padding the zeros
+            string resultPaddZerose = resultAinBinary.PadLeft(10, '0');
+            //converting B 
+            string resultB=String.Format("{0:F2}",numberB);
+
+            //converting C 
+            string resultC = String.Format("{0:F3}", numberC);
+
+            Console.Write("Number A= {0} Number B={1} number C={2}", numberA,numberB,numberC);
+            Console.Write("\n");
+            Console.WriteLine(String.Format("{0,-5} | {1,5} | {2,10} | {3,-10} | ", resultHexa, resultPaddZerose, resultB, resultC));
+
             Console.ReadLine();
 
         }
